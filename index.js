a4543f92962bfae5441fd297991c40c934910a53
Amar <amarshissal224@gmail.com>
2025-08-27 14:33:26 +0530

    Changed config to .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const config = require("config");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,7 +11,7 @@ app.use(express.json());
 
 // Connect to MongoDB Atlas
 mongoose
-  .connect(config.get("db"), {
+  .connect(process.env.db, {
     // options not needed in latest driver
   })
   .then(() => console.log("✅ MongoDB connected"))
